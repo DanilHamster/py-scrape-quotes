54439dc3dfa4d46f2703b01e5b25b54b7b8a0802
Hamster <dtaranenko137@gmail.com>
2025-10-19 15:16:14 +0300

    'Solution'

--- app/parse.py
+++ app/parse.py
@@ -1,4 +1,8 @@
-from dataclasses import dataclass
+import csv
+from dataclasses import dataclass, fields, astuple
+
+import requests
+from bs4 import BeautifulSoup, Tag
 
 
 @dataclass
@@ -7,9 +11,50 @@ class Quote:
     author: str
     tags: list[str]
 
+PRODUCT_FIELDS = [field.name for field in fields(Quote)]
+
+BASE_URL = "https://quotes.toscrape.com/"
+
+def parse_product(quote: Tag) -> Quote:
+    return Quote(
+        text=quote.select_one(".text").text,
+        author=quote.select_one(".author").text,
+        tags=[tag.text for tag in quote.select(".tag")]
+    )
+
+def get_single_quote(soup):
+    quotes = soup.select(".quote")
+    return [parse_product(quote) for quote in quotes]
+
+def get_page_nums():
+    page = 1
+    while True:
+        text = requests.get(BASE_URL + f"page/{page}").content
+        soup = BeautifulSoup(text, "html.parser")
+        if not soup.select(".next"):
+            break
+        page += 1
+    return page
+
+def get_parses():
+    all_quotes = []
+    page_num = get_page_nums()
+    for page in range(1, page_num + 1):
+        text = requests.get(BASE_URL + f"page/{page}").content
+        soup = BeautifulSoup(text, "html.parser")
+        all_quotes.extend(get_single_quote(soup))
+    return all_quotes
+
+
+def write_to_csv(quotes: [Quote], output_csv_path: str) -> None:
+    with open(output_csv_path, "w", newline="", encoding="utf-8") as f:
+
+        writer = csv.writer(f)
+        writer.writerow(PRODUCT_FIELDS)
+        writer.writerows([astuple(quote) for quote in quotes])
 
 def main(output_csv_path: str) -> None:
-    pass
+    write_to_csv(get_parses(), output_csv_path)
 
 
 if __name__ == "__main__":
